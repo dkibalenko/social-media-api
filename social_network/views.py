211c24338b096a57c266e4d8624dfda8b3302d54
Denys Kibalenko <dennieking87@gmail.com>
2024-12-17 19:37:27 +0100

    Feat: add CurrentUserProfileView & ProfileViewSet to social_network/views.py

--- social_network/views.py
+++ social_network/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework import mixins
 
-# Create your views here.
+from social_network.models import Profile
+from social_network.serializers import ProfileSerializer
+
+
+class CurrentUserProfileView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        user_profile = generics.get_object_or_404(
+            Profile,
+            user=self.request.user
+        )
+        return user_profile
+
+    def destroy(self, request, *args, **kwargs):
+        profile = self.get_object()
+        user = profile.user
+        response = super().destroy(request, *args, **kwargs)
+        user.delete()
+        return response
+
+
+class ProfileViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
