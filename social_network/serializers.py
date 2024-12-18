ee0147e3f14907a48471025f39d6f4a7b1c2be2d
Denys Kibalenko <dennieking87@gmail.com>
2024-12-18 13:35:45 +0100

    Feat: add `update` method to ProfileSerializer to social_network/serializers.py

--- social_network/serializers.py
+++ social_network/serializers.py
@@ -20,6 +20,14 @@ class ProfileSerializer(serializers.ModelSerializer):
             "bio",
         )
 
+    def update(self, instance, validated_data):
+        """"
+        If no new profile image in the request, keep the existing one.
+        """
+        if not validated_data["profile_image" ]:
+            validated_data["profile_image"] = instance.profile_image
+        return super().update(instance, validated_data)
+
 
 class ProfileListSerializer(serializers.ModelSerializer):
     class Meta:
