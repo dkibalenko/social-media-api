e7ebb5629d73e0c3158bb5637be25542119f4bc7
Denys Kibalenko <dennieking87@gmail.com>
2025-01-02 17:11:06 +0100

    Feat: add condition for using permission in `ProfileViewSet` in social_network/permissions.py

--- social_network/permissions.py
+++ social_network/permissions.py
@@ -6,5 +6,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         if request.user and request.user.is_authenticated:
+            if view.__class__.__name__ == "ProfileViewSet":
+                return obj.user.profile == request.user.profile
             return obj.author == request.user.profile
         return False
