52dee15f9560b736a8c4519d5e96e0102fd1688e
Denys Kibalenko <dennieking87@gmail.com>
2025-01-04 13:23:35 +0100

    Feat: add url endpoint for `SpectacularAPIView`, `SpectacularSwaggerView`, `SpectacularRedocView` to social_media_api/urls.py

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
@@ -25,7 +33,16 @@ urlpatterns = [
     path("api/user/", include("user.urls", namespace="user")),
     path(
         "api/social_network/",
-        include("social_network.urls", namespace="social_network")
+        include("social_network.urls", namespace="social_network"),
+    ),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
