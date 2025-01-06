eb87d2ed5c902a5b6cc31ccf289899640d4c2211
Denys Kibalenko <dennieking87@gmail.com>
2025-01-06 19:24:23 +0100

    Chore: formatting and adding docstirngs to files

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -42,7 +42,9 @@ urlpatterns = [
         name="swagger-ui",
     ),
     path(
-        "api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- social_network/signals.py
+++ social_network/signals.py
@@ -7,10 +7,10 @@ from .models import Profile
 User = get_user_model()
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs) -> None:
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
+def save_user_profile(sender, instance, **kwargs) -> None:
     instance.profile.save()
