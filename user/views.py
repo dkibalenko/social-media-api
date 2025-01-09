c45e553af7ae9d01fc41bde976b6c7f77a5f7556
Denys Kibalenko <dennieking87@gmail.com>
2025-01-09 17:27:53 +0100

    Fix: add `permission_classes = (AllowAny,)` to `CreateUserView` in user/views.py

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.views import TokenBlacklistView
 from rest_framework.response import Response
@@ -9,6 +9,7 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
